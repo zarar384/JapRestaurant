06aa671c8a2afd75f95e9ac068de781752dcfc7b
zarar384 <fel98_98@mail.ru>
2022-06-27 22:00:42 +0200

    Add project files.

---
+++ Jap.Services.ProductAPI/DbContexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Jap.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jap.Services.ProductAPI.DbContexts
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Product> Products{ get; set; }
+    }
+}
---
+++ Jap.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jap.Services.ProductAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1,1000)]
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
