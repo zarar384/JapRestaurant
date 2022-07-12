5bc05a998043b494cf81b4c52e777b6bae50d25d
zarar384 <fel98_98@mail.ru>
2022-07-12 17:40:37 +0200

    add customizeble options ProfileServise

---
+++ Jap.Services.Identity/Services/ProfileService.cs
@@ -0,0 +1,61 @@
+﻿using Duende.IdentityServer.Extensions;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityModel;
+using Jap.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Jap.Services.Identity.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IUserClaimsPrincipalFactory<ApplicationUser> _userClaimsPrincipalFactory;
+        private readonly UserManager<ApplicationUser> _userMgr;
+        private readonly RoleManager<IdentityRole> _roleMgr;
+
+        public ProfileService(IUserClaimsPrincipalFactory<ApplicationUser> userClaimsPrincipalFactory, UserManager<ApplicationUser> userMgr, RoleManager<IdentityRole> roleMgr)
+        {
+            _userClaimsPrincipalFactory = userClaimsPrincipalFactory;
+            _userMgr = userMgr;
+            _roleMgr = roleMgr;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            string sub = context.Subject.GetSubjectId();
+            ApplicationUser user = await _userMgr.FindByIdAsync(sub);
+            ClaimsPrincipal userClaims = await _userClaimsPrincipalFactory.CreateAsync(user);
+
+            List<Claim> claims = userClaims.Claims.ToList();
+            claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
+
+            claims.Add(new Claim(JwtClaimTypes.FamilyName, user.LastName));
+            claims.Add(new Claim(JwtClaimTypes.GivenName, user.FirstName));
+
+            if (_userMgr.SupportsUserRole)
+            {
+                IList<string> roles = await _userMgr.GetRolesAsync(user);
+                foreach(var rolename in roles)
+                {
+                    claims.Add(new Claim(JwtClaimTypes.Role, rolename));
+                    if (_roleMgr.SupportsRoleClaims)
+                    {
+                        IdentityRole role = await _roleMgr.FindByNameAsync(rolename);
+                        if (role != null)
+                        {
+                            claims.AddRange(await _roleMgr.GetClaimsAsync(role));
+                        }
+                    }
+                }
+            }
+        }
+         
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            string sub = context.Subject.GetSubjectId();
+            ApplicationUser user = await _userMgr.FindByIdAsync(sub);
+            context.IsActive = user != null;
+        }
+    }
+}
--- Jap.Web/Controllers/HomeController.cs
+++ Jap.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Jap.Web.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -31,8 +32,9 @@ namespace Jap.Web.Controllers
         }
 
         [Authorize]
-        public IActionResult Login()
+        public async Task<IActionResult> Login()
         {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
             return RedirectToAction(nameof(Index));
         }
 
